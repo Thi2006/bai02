2801003271aeb09dc97131f3d6d680baa0395ddb
Thi2006 <104618700+Thi2006@users.noreply.github.com>
2022-05-25 13:54:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<h1>Ngôn ngữ lập trình PHP</h1>
+	<?php echo "Hello World";?>	
+	<?php
+		$language = 'PHP';
+		var_dump($language);
+	?>
+	<footer>Footer</footer>
+
+
+
+
+
+</body>
+</html>
